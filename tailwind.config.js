9ac86787dc679fd157c144540156be20c757b49a
Ben Furfie <hello@benfurfie.co.uk>
2024-02-24 17:41:10 +0000

    Add safelist colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ export default {
         './resources/**/*.vue',
         './content/**/*.md',
     ],
-
+    safelist: [
+        'bg-orange-500',
+        'text-orange-500',
+        'bg-purple-500',
+        'text-purple-500',
+        'bg-red-500',
+        'text-red-500',
+    ],
     theme: {
         extend: {},
     },
